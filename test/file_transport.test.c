06542c8787e2f61ac2f069e74a72247802375741
vikash <vikash.ckiiita30@gmail.com>
2016-12-25 13:45:47 +0530

    Update severity level to debug

--- test/file_transport.test.c
+++ test/file_transport.test.c
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2016 Vikash Kesarwani
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,6 +30,7 @@ void test_file_transport()
     clogger* logger = clogger_init();
 
     file_transport_t *file_transport = file_transport_new("/tmp/test.log");
+    file_transport_setopt(file_transport, TRANSPORT_OPT_SEVERITY, SEVERITY_DEBUG);
 
     clogger_add_transport(logger, (transport_t*)file_transport);
 

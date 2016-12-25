936c29ddf86686aad9fb911fdaf09b1e5f9b20d0
vikash <vikash.ckiiita30@gmail.com>
2016-12-25 14:08:31 +0530

    Add test for updating default logger severity level

--- test/default_logger.test.c
+++ test/default_logger.test.c
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2016 Vikash Kesarwani
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,7 +37,26 @@ void test_default_logger()
     clogger_debugd("Debug type log");
 }
 
+void test_default_logger_change_severity()
+{
+    extern clogger *default_logger;
+    console_transport_t* transport = (console_transport_t*)clogger_get_transport(default_logger, "console");
+    if (transport != NULL)
+    {
+        console_transport_setopt(transport, TRANSPORT_OPT_SEVERITY, SEVERITY_DEBUG);
+    }
+    clogger_emergd("Emergency type log %s", "arg 1");
+    clogger_alertd("Alert type log");
+    clogger_critd("Ciritcal type log");
+    clogger_noticed("Notice type log");
+    clogger_infod("Info type log %s", "arg 1");
+    clogger_warnd("Warning type log");
+    clogger_errord("Error type log");
+    clogger_debugd("Debug type log");
+}
+
 int main()
 {
     test_default_logger();
+    test_default_logger_change_severity();
 }
